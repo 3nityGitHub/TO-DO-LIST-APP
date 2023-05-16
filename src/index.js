56beaa215093b4942a5c2c8f97910223111e5038
3nityGitHub <60592651+3nityGitHub@users.noreply.github.com>
2023-05-16 22:26:50 +0100

    Install style loader and css loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-
+import './style.css';
 import printMe from './print.js';
 
 function component() {
@@ -8,7 +8,7 @@ function component() {
   const btn = document.createElement('button');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+  element.classList.add('hello');
   btn.innerHTML = 'Click me and check the console!';
 
   btn.onclick = printMe;
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,23 @@ module.exports = {
     index: './src/index.js',
     print: './src/print.js',
   },
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
 
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+        type: 'asset/resource',
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Output Management',
@@ -18,5 +34,8 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
 };
